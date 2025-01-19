15d6d0aef80156f502a217886728c032c9da76be
Jabal Surya Ngalam <87789231+Jabalsurya2105@users.noreply.github.com>
2025-01-19 21:28:54 +0700

    Add files via upload

---
+++ data/script-vip.js
@@ -0,0 +1,77 @@
+{
+  "developer": "SuryaDev",
+  "number": "62895415497664",
+  "information": [
+  {
+    "code": "F.U",
+    "meaning": "Free Update"
+  },
+  {
+    "code": "N.F.U",
+    "meaning": "No Free Update"
+  },
+  {
+    "code": "A.F",
+    "meaning": "Additional Features"
+  },
+  {
+    "code": "B.L",
+    "meaning": "Blacklisted"
+  },
+  {
+    "code": "O.B",
+    "meaning": "Open Blacklist"
+  },
+  {
+    "code": "F.U.P",
+    "meaning": "Free Update Promo"
+  },
+  {
+    "code": "N.U.P",
+    "meaning": "No Update Promo"
+  },
+  {
+    "code": "C.A",
+    "meaning": "Create Akun"
+  },
+  ],
+  "result": [
+    "Zidan ➠ 370k *(100 F.U + 270 A.F)*",
+    "Epul ➠ 335k *(150 F.U + 185 A.F)*",
+    "Fourstore ➠ 310k *(100 F.U + 210 A.F)*",
+    "Aji ➠ 305k *(100 F.U + 205 A.F)*",
+    "Asep ➠ 210k *(100 F.U + 110 A.F)*",
+    "Reza ➠ 205k *(100 F.U + 105 A.F)*",
+    "Zora ➠ 200k *(150 F.U + 50 O.B)*",
+    "Exyu ➠ 150k *(110 F.U + 40 A.F)*",
+    "Ryan ➠ 150k *(100 F.U + 50 C.A)*",
+    "Wahyu ➠ 150k *(F.U)*",
+    "Azaa ➠ 100k *(F.U)*",
+    "Iky ➠ 100k *(F.U)*",
+    "Fachri ➠ 100k *(F.U)*",
+    "Nikola ➠ 100k *(F.U)*",
+    "Nathan ➠ 100k *(F.U)*",
+    "Fahri ➠ 100k *(F.U)*",
+    "Ashraf ➠ 100k *(F.U)*",
+    "Yann ➠ 100k *(F.U)*",
+    "Yuliaw ➠ 100k *(F.U)*",
+    "Lexxy ➠ 100k *(F.U)*",
+    "Syauqi ➠ 100k *(B.L)*",
+    "Nata ➠ 100k *(F.U)*",
+    "Ikyu ➠ 100k *(F.U)*",
+    "Aidil ➠ 100k *(F.U)*",
+    "Canopos ➠ 100k *(F.U)*",
+    "Lewis ➠ 100k *(F.U)*",
+    "Ganes ➠ 100k *(F.U)*",
+    "Tomi ➠ 100k *(F.U)*",
+    "Rann ➠ 100k *(F.U)*",
+    "Dikzz ➠ 100k *(F.U)*",
+    "Wildan ➠ 100k *(F.U)*",
+    "Zaghi ➠ 100k *(F.U)*",
+    "Bintang ➠ 90k *(N.F.U)*",
+    "Irull ➠ 80k *(F.U.P)*",
+    "Adzan ➠ 80k *(F.U.P)*",
+    "Khohar ➠ 70k *(F.U.P)*",
+    "Alawy ➠ 70k *(N.U.P)*"
+  ]
+}
